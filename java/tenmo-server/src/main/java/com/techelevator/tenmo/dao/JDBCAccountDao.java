a6d580a0b64f0271ecdbd12844308c3da6429e46
Victoria Laird <victorialaurie90@gmail.com>
2021-07-01 19:32:44 -0400

    Trying to fix the DAOs

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCAccountDao.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCAccountDao.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+//Life is pain
 @Component
 public
 class JdbcAccountDao implements AccountDao {
--- java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCTransferDao.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCTransferDao.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+//Life is still pain
 @Component
 public
 class JdbcTransferDao implements TransferDao {
